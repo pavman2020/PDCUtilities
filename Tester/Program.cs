194fe6bd0e08f4a3f034b2c14c22e0f43170b2e0
pavman2020 <pavman2008@gmail.com>
2021-10-20 09:37:03 -0400

    add more utilities

--- Tester/Program.cs
+++ Tester/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using static PDCUtility.TypeConverters;
+using static PDCUtility.FractionConverter;
 
 namespace PDCUtilitiesTester
 {
@@ -6,6 +8,13 @@ namespace PDCUtilitiesTester
     {
         static void Main(string[] args)
         {
+
+            foreach (double d in new double[] { 123.375, 99.5, 99.51, 12.625, 673.432, 66.231, 0.998, 3.14159 })
+            {
+                Console.WriteLine(string.Format("{0} = {1} = '{2}' = '{3}'", nameof(d), d, d.GetFractionStringFromDouble(), ((decimal)d).ConvertDecimalToFraction(true)));
+            }
+
+
             Console.WriteLine("Hello World!");
         }
     }
